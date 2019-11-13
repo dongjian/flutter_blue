0d4f67b0e81d0cc62b5d47f5159809daa20d83de
dongjian <381423609@qq.com>
2019-11-13 16:42:34 +0800

    ----

--- lib/src/bluetooth_characteristic.dart
+++ lib/src/bluetooth_characteristic.dart
@@ -123,7 +123,7 @@ class BluetoothCharacteristic {
         .invokeMethod('writeCharacteristic', request.writeToBuffer());
 
     if (type == CharacteristicWriteType.withoutResponse) {
-      _value.add(value);
+      //_value.add(value);
       return result;
     }
 
